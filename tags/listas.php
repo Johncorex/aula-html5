f46439d81a69f64c3ea0f403e3a6c74aae6b2e61
Johncore <32520764+Johncorex@users.noreply.github.com>
2021-09-24 23:52:28 -0300

    listas

---
+++ tags/listas.php
@@ -0,0 +1,12 @@
+<ul> 
+item 1
+item 2
+<!-- é usado para itens que não tem importancia de estar em ordem -->
+
+<ol>
+1. item 1
+2. item 3
+<!-- a ordem dos itens é importante -->
+
+<li>
+<!-- é um item dessa lista -->
